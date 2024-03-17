08826b3cebff05ba76dccd5008dafc4e7afa9c37
Vadym Manchenko <clashstarset2019@gmail.com>
2024-03-17 11:57:50 +0200

    Add unversional files

---
+++ BookStore.Memory/BookRepository.cs
@@ -0,0 +1,22 @@
+﻿using BookStore.Entities;
+using BookStore.Interfaces;
+using System.Linq;
+
+namespace BookStore.Memory;
+
+public class BookRepository : IBookRepository
+{
+    private readonly Book[] books = new[]
+    {
+        new Book(1, "Book1"),
+        new Book(2, "Book2"),
+        new Book(3, "Book3"),
+    };
+
+    public Book[] GetAllByTitle(string titlePart)
+    {
+        return books.Where(
+            book => book.Title
+                .Contains(titlePart)).ToArray();
+    }
+}
